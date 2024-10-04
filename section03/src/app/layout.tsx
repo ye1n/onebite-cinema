6f465e640bea54dc4da7742c4d4a3e94b23f131b
ye1n <uyeinyein@gmail.com>
2024-10-04 10:49:28 +0900

    feat: 9일차 과제 구현

--- section03/src/app/layout.tsx
+++ section03/src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        Global Layout
         {children}
       </body>
     </html>
---
+++ section03/src/app/movie/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function Page({
+  params,
+}: {
+  params: { id: string | string[] };
+}) {
+  return <div>movie : {params?.id}</div>;
+}
